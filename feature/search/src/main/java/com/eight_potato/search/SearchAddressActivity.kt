eb482e79bbf89da6369d6470cf9faa6692939087
aurora32s <aurora32s@naver.com>
2024-07-21 11:22:44 +0900

    [FEATURE] #1 - 현재 위치 검색 버튼 추가

--- feature/search/src/main/java/com/eight_potato/search/SearchAddressActivity.kt
+++ feature/search/src/main/java/com/eight_potato/search/SearchAddressActivity.kt
@@ -1,11 +1,22 @@
 package com.eight_potato.search
 
 import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.eight_potato.designsystem.input.HyusikOutlinedTextField
@@ -37,6 +48,7 @@ class SearchAddressActivity : BaseActivity() {
                     onValueChanged = viewModel::changeKeyword,
                     onClear = { viewModel.changeKeyword("") }
                 )
+                CurrentPositionButton {}
                 SearchAddressList(
                     keyword = "홍대",
                     addresses = TEST_ADDRESS,
@@ -44,5 +56,27 @@ class SearchAddressActivity : BaseActivity() {
                 )
             }
         }
+    }
+}
+
+@Composable
+private fun CurrentPositionButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            imageVector = Icons.Default.Search,
+            contentDescription = "현재 위치로 주소 검색"
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = "현재 위치로 주소 찾기")
     }
 }
